ec0fa5ea6ee798b29b244edfded64eca43df7aee
Eli Hellmann <ESHellmann@NYC-79DN2R1-L.wanlink.us>
2013-05-31 17:37:41 -0400

    adding models

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+#The environment variable DATABASE_URL should be in the following format:
+# => postgres://{user}:{password}@{host}:{port}/path
+
 # Initialize the rails application
 RailsSiteTest::Application.initialize!
