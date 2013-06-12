94fdd0d56d4cc26595dc067b9543828c1a86aace
unknown <fragierus@gmail.com>
2013-06-11 22:46:44 -0400

    hooking outputs into form (for pollyfill)

--- app/assets/javascripts/pollyfillOutput.js
+++ app/assets/javascripts/pollyfillOutput.js
@@ -13,6 +13,15 @@ window.addEventListener("load", function(event){
         };
         Object.defineProperty(outputs[i],'value',propDesc);
       }
+      
+      var forms = document.getElementsByTagName('form');
+      for(var i = 0; i < forms.length; i++){
+        var outputs = forms[i].getElementsByTagName('output');
+        for(var j = 0; j < outputs.length; j++){
+          if(outputs[j].hasAttribute('name'))
+            forms[i][outputs[j].getAttribute('name')] = outputs[j];
+        }
+      }
     }  
 },false)
   
