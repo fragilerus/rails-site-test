1c498c4cff73f815562e7e0c53f358f19d904e0b
Eli Hellmann <fragilerus@gmail.com>
2013-06-19 19:47:10 -0400

    moved everything out of publice. changed layout to be blank, added default route to new application view (outside angular shell).

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,5 @@
+var mainSite = angular.module('mainSite', [])
+  		.config(function($routeProvider){
+    		$routeProvider.when('/oldIndex',{ templateUrl: '_index.html'})
+            		      .when('/mortgage_calculator', { templateUrl: 'mortgage_calculator/index'});
+  		});
--- public/js/app.js
+++
@@ -1,5 +0,0 @@
-var mainSite = angular.module('mainSite', [])
-  .config(function($routeProvider){
-    $routeProvider.when('/oldIndex',{ templateUrl: '_index.html'})
-                  .when('/mortgage_calculator', { templateUrl: 'mortgage_calculator/index'});
-  });
