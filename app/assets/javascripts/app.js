f333e87fdafa0777fb3f738159914e0bd83217ef
Eli Hellmann <fragilerus@gmail.com>
2013-06-20 19:36:19 -0400

    changinng angular notation to avoid minification issues

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,5 +1,5 @@
 var mainSite = angular.module('mainSite', [])
-  		.config(function($routeProvider){
+  		.config(["$routeProvider", function($routeProvider){
     		$routeProvider.when('/oldIndex',{ templateUrl: '_index.html'})
             		      .when('/mortgage_calculator', { templateUrl: 'mortgage_calculator/index'});
-  		});
+  		}]);
