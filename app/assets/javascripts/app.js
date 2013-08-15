e7c76c7b919a1549b3e4fabc26e49cf12103fe29
unknown <fragierus@gmail.com>
2013-08-14 23:30:54 -0400

    adding http

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,5 +1,6 @@
 var mainSite = angular.module('mainSite', [])
   		.config(["$routeProvider", function($routeProvider){
-    		$routeProvider.when('/oldIndex',{ templateUrl: '_index.html'})
+    		$routeprovider.when('/oldindex',{ templateurl: '_index.html'})
+    					  .when('/http',{ templateurl: '_index.html'})
             		      .when('/mortgage_calculator', { templateUrl: 'mortgage_calculator/index'});
   		}]);
